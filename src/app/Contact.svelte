4b41cfc71a888299a98e14ad23e9b16f070fa458
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-06-30 01:21:58 +0530

    fixed contact button in light mode

--- src/app/Contact.svelte
+++ src/app/Contact.svelte
@@ -10,7 +10,7 @@
   <div slot="body" class="mt-4 flex justify-center mb-16 sm:mb-32">
     <a href={ContactData.ctaLink} target="_blank" rel="noopener noreferrer">
       <button
-        class="dark:bg-black bg-white dark:text-white text-black px-4 py-2 rounded-md flex flex-row justify-center h-16 w-40 gap-1 items-center"
+        class="dark:bg-black bg-gray-900/50 dark:text-white text-black px-4 py-2 rounded-md flex flex-row justify-center h-16 w-40 gap-1 items-center"
       >
         <h1 class="text-xl">{ContactData.ctaText}</h1>
       </button>
